f7a46301b63e97850afe024cfac76969c266a068
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-02-14 13:22:37 +0000

    Test

--- Field.java
+++ Field.java
@@ -8,7 +8,7 @@ import java.util.Random;
  * Represent a rectangular grid of field positions.
  * Each position is able to store a single animal.
  *
- * @author David J. Barnes and Michael
+ * @author David J. Barnes
  * @version 2016.02.29
  */
 public class Field
